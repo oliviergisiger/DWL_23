17311270f9bcadbd5dd696492ebb3da81894d46f
oliviergisiger <oliviergisiger@bluewin.ch>
2023-03-23 12:40:46 +0100

    initialized wa source adapter

--- app/weather_data/adapters/weather_data_source.py
+++ app/weather_data/adapters/weather_data_source.py
@@ -1,19 +1,24 @@
 from weather_data.ports.weather_data import WeatherDataSource
 from airflow.hooks.S3_hook import S3Hook
-
+import panadas as pd
 
 class WeatherDataSourceAdapter(WeatherDataSource):
     """
     should fetch newes file from S3 / minio and transform it
     should inherit from an abstract class
     """
-    def __init__(self):
-        pass
+    def __init__(self, connection):
+        self._connection = connection
+
 
 
 
     def read_file(self, filename: str) -> pd.DataFrame:
+        pass
 
+    def _build_source_file_system(self):
+        s3 = S3Hook(self._connection)
+        s3.
 
 
 
