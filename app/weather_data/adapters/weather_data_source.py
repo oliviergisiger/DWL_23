02010011b447c0fef20a15ea2427e3710aa85709
oliviergisiger <oliviergisiger@bluewin.ch>
2023-03-23 11:46:12 +0100

    [feature] initial wd part deliver

--- app/weather_data/adapters/weather_data_source.py
+++ app/weather_data/adapters/weather_data_source.py
@@ -1,10 +1,21 @@
+from weather_data.ports.weather_data import WeatherDataSource
+from airflow.hooks.S3_hook import S3Hook
 
-class WeatherDataSource:
+
+class WeatherDataSourceAdapter(WeatherDataSource):
     """
     should fetch newes file from S3 / minio and transform it
     should inherit from an abstract class
     """
-    pass
+    def __init__(self):
+        pass
+
+
+
+    def read_file(self, filename: str) -> pd.DataFrame:
+
+
+
 
 
 if __name__ == '__main__':
