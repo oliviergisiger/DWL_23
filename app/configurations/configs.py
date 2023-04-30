988b3d68933bc42edaf48b5a466d52e2d25f8962
oliviergisiger <oliviergisiger@bluewin.ch>
2023-04-30 12:06:01 +0200

    initialized environment class for development and production

--- app/configurations/configs.py
+++ app/configurations/configs.py
@@ -1,13 +1,32 @@
 from datetime import datetime
+from dataclasses import dataclass
 
-
-# Sets variables for local development environment
-
-
-
+#
 START_DATE = datetime(2023, 3, 25)
 END_DATE = datetime(2023, 3, 31)
 
+
+@dataclass
+class Environment:
+    environment: str
+    connections: dict
+    dag_start_date: datetime
+    dag_end_data: datetime = None
+
+
+LOCAL_DEV = Environment(
+    environment='LOCAL_DEV',
+    connections={'S3': 'S3_DEVELOPMENT'},
+    dag_start_date=datetime(2023, 3, 25),
+    dag_end_data=datetime(2023, 3, 31)
+)
+
+PRODUCTION = Environment(
+    environment='PRODUCTION',
+    connections={'S3': 'S3_PRODUCTION'},
+    dag_start_date=datetime(2023, 3, 25),
+    dag_end_data=None
+)
 
 
 
